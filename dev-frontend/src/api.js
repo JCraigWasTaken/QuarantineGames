07fee9ad1f1c45e8d181cf43718f4d370732e72a
Chris Bentz <christopher@bentz.org>
2020-04-17 10:32:19 -0400

    Another try.

--- dev-frontend/src/api.js
+++ dev-frontend/src/api.js
@@ -1,6 +1,11 @@
 const openSocket = require('socket.io-client');
 const port = process.env.PORT || 5000;
-const socket = openSocket(`http://localhost:${port}`);
+let socket;
+if (process.env.NODE_ENV === 'production') {
+  socket = openSocket();
+} else {
+  socket = openSocket(`http://localhost:${port}`);
+}
 
 function writeMessage(message) {
   socket.emit('writeMessage', message);
