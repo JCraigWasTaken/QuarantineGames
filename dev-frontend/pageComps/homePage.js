14aa7afe9ce596f5bee113b2b71c7ec6c42f4921
John Craig <john22.craig@gmail.com>
2020-05-11 00:19:15 -0400

    Revampe Buttons and Layout

--- dev-frontend/pageComps/homePage.js
+++ dev-frontend/pageComps/homePage.js
@@ -3,10 +3,28 @@ import React from 'react';
 class HomePage extends React.Component {
     render(){
         return(
-            <div className='flex-column flex_center flex_spaceCenter'>
-                <h1>Pick a game</h1>
-                <a href={window.location.href+"death-roll"} className='submitButton'>Death Roll</a>
-                <a href={window.location.href+"death-box"} className='submitButton'>Death Box</a>
+            <div className='flex-column flex_center margin_vertical50'>
+                <div className='flex-row flex_spacebetween' style={{'width':'74%'}}>
+                    <div style={{'width':'50%'}}>
+                        <h1 className='color-text_dominant' style = {{'margin-top':'30px'}}>Pick a game</h1>
+                    </div>
+                    <div className='flex-column' style={{'width':'50%'}}>
+                        <div className='homePageButtonRow'>
+                            <a href={window.location.href+"death-roll"} className='homePageButton'>Death Roll</a>
+                            <div id='arrow'>
+                                <div id='arrowBody'>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='homePageButtonRow'>
+                            <a href={window.location.href+"death-box"} className='homePageButton'>Death Box</a>
+                            <div id='arrow'>
+                                <div id='arrowBody'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
