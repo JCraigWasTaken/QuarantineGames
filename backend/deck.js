994cf69334b1d53d1fbb9addd2722207c8c907b7
Chris Bentz <christopher@bentz.org>
2020-05-15 15:51:19 -0400

    Add all cards to deck.

--- backend/deck.js
+++ backend/deck.js
@@ -1,5 +1,5 @@
 class Deck {
-	constructor(suits = ['spade', 'club', 'heart', 'diamond'], numbers = ['A', '2', '3', '4', '5']/*, '6', '7', '8', '9', '10', 'J', 'Q', 'K']*/) {
+	constructor(suits = ['spade', 'club', 'heart', 'diamond'], numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']) {
 		this.cards = this.generateCards(suits, numbers);
 	}
 
